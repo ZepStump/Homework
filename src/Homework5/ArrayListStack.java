8e9cd37bd6b7d6238a33d3122f3d3eec2d79bc72
ZepStump <HeadmenBrothers@yandex.ru>
2020-10-31 20:43:02 -0700

    Final final stack change

--- src/Homework5/ArrayListStack.java
+++ src/Homework5/ArrayListStack.java
@@ -1,94 +1,42 @@
 package Homework5;
 
+import java.util.List;
+import java.util.ArrayList;
 import java.util.EmptyStackException;
-/** Implementation of the interface StackInt<E> using
- *  an array.
- */
+
 public class ArrayListStack<E> implements StackInterface<E> {
 
-    // Data Fields
-    /** Storage for stack. */
-    E[] theData;
-    /** Index to top of stack. */
-    int topOfStack = -1; // Initially empty stack.
-    private static final int INITIAL_CAPACITY = 10;
+    private List<E> theData;
 
-    /**
-     * Construct an empty stack with the default
-     * initial capacity.
-     */
     public ArrayListStack() {
-        theData = (E[]) new Object[INITIAL_CAPACITY];
+        theData = new ArrayList<E>();
     }
 
-    /**
-     * Insert a new item on top of the stack.
-     * @post The new item is the top item on the stack.
-     *       All other items are one position lower.
-     * @param obj The item to be inserted
-     * @return The item that was inserted
-     */
     @Override
     public E push(E obj) {
-        if (topOfStack == theData.length - 1) {
-            reallocate();
-        }
-        topOfStack++;
-        theData[topOfStack] = obj;
+        theData.add(obj);
         return obj;
     }
 
-    /**
-     * Remove and return the top item on the stack.
-     * @pre The stack is not empty.
-     * @post The top item on the stack has been
-     *       removed and the stack is one item smaller.
-     * @return The top item on the stack
-     * @throws EmptyStackException if the stack is empty
-     */
-    @Override
-    public E pop() {
-        if (empty()) {
-            throw new EmptyStackException();
-        }
-        return theData[topOfStack--];
-    }
-
-    /*<exercise chapter="3" section="3" type="programming" number="2">*/
-    /**
-     * Return the top item on the stack
-     * Pre: The stack is not empty
-     * Post: The stack remains unchanged
-     * @return The top item on the stack
-     * @throws EmptyStackException If the stack
-     *         is empty
-     */
     @Override
     public E peek() {
         if (empty()) {
             throw new EmptyStackException();
         }
-        return theData[topOfStack];
+        return theData.get(theData.size() - 1);
     }
 
-    /**
-     * Return true if the stack is empty
-     * @return True if the stack is empty
-     */
+    @Override
+    public E pop() {
+        if (empty()) {
+            throw new EmptyStackException();
+        }
+        return theData.remove(theData.size() - 1);
+    }
+
     @Override
     public boolean empty() {
-        return topOfStack == -1;
-    }
-
-    /**
-     * Method to reallocate the array containing the stack data
-     * @post The size of the data array has been doubled
-     *       and all of the data has been copied to the new array
-     */
-    private void reallocate() {
-        E[] temp = (E[]) new Object[2 * theData.length];
-        System.arraycopy(theData, 0, temp, 0, theData.length);
-        theData = temp;
+        return theData.size() == 0;
     }
 }
 
